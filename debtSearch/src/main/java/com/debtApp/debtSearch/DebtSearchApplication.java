534cdb650902e66767465ba0636a2f7749bb8664
kweckolukasz <kweckolukasz@gmail.com>
2021-05-14 11:22:18 +0200

    Configure: create microservice for searching

---
+++ debtSearch/src/main/java/com/debtApp/debtSearch/DebtSearchApplication.java
@@ -0,0 +1,59 @@
+package com.debtApp.debtSearch;
+
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import org.apache.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+@SpringBootApplication
+//Disabled in yml
+@EnableDiscoveryClient
+@EnableCaching
+public class DebtSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DebtSearchApplication.class, args);
+    }
+
+    @Autowired
+    private ReactorLoadBalancerExchangeFilterFunction lbFunction;
+
+    @Configuration
+    @LoadBalancerClient(name = "debtSearch")
+    class WebClientConfig {
+
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .responseTimeout(Duration.ofMillis(5000))
+                .doOnConnected(connection ->
+                        connection.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS))
+                                .addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS)));
+
+        @LoadBalanced
+        @Bean
+        WebClient.Builder webclientBuilder() {
+            return WebClient.builder()
+                    .baseUrl("http://debtSearch")
+                    .filter(lbFunction)
+                    .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                    .clientConnector(new ReactorClientHttpConnector(httpClient));
+        }
+    }
+}
---
+++ debtSearch/src/test/java/com/debtApp/debtSearch/DebtSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debtApp.debtSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebtSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
